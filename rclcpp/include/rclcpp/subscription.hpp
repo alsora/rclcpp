bdaf8ddeabfc6e45ec5b72c0320073834c1b2eb7
alberto <asoragna@irobot.com>
2019-05-14 10:42:01 +0100

    added create waitable and create threads

--- rclcpp/include/rclcpp/subscription.hpp
+++ rclcpp/include/rclcpp/subscription.hpp
@@ -231,7 +231,7 @@ public:
     return intra_process_subscription_handle_;
   }
 
-  #if IPC_TYPE == IPC_TYPE_SPIN
+  #if IPC_TYPE == IPC_TYPE_QUEUE_SPIN
   std::shared_ptr<rclcpp::Waitable>
   create_intra_process_waitable()
   {
@@ -299,7 +299,7 @@ private:
   std::shared_ptr<moodycamel::BlockingConcurrentQueue<ConstMessageSharedPtr> > typed_queue;
   #endif
 
-  #if IPC_TYPE == IPC_TYPE_SPIN
+  #if IPC_TYPE == IPC_TYPE_QUEUE_SPIN
   std::shared_ptr<IPCSubscriptionWaitable<CallbackMessageT, Alloc>> waitable_ptr;
   #endif
 
